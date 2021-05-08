19da5b094921625d4dcae3e509dd18ea1a5d5d5f
Dmitry Yakovlev <web-dimayakovlev@outlook.com>
2021-05-08 13:40:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,11 +12,11 @@
 /* pre-common setup, load gsconfig and get GSADMIN path */
 
 	/* GSCONFIG definitions */
-	if(!defined('GSFRONT')) define('GSFRONT',1);
-	if(!defined('GSBACK'))  define('GSBACK',2);
-	if(!defined('GSBOTH'))  define('GSBOTH',3);	
-	if(!defined('GSSTYLEWIDE')) define('GSSTYLEWIDE','wide'); // wide style sheet
-	if(!defined('GSSTYLE_SBFIXED')) define('GSSTYLE_SBFIXED','sbfixed'); // fixed sidebar
+	if(!defined('GSFRONT')) define('GSFRONT', 1);
+	if(!defined('GSBACK')) define('GSBACK', 2);
+	if(!defined('GSBOTH')) define('GSBOTH', 3);
+	if(!defined('GSSTYLEWIDE')) define('GSSTYLEWIDE', 'wide'); // wide style sheet
+	if(!defined('GSSTYLE_SBFIXED')) define('GSSTYLE_SBFIXED', 'sbfixed'); // fixed sidebar
 
 	# Check and load gsconfig
 	if (file_exists('gsconfig.php')) {
@@ -32,8 +32,8 @@
 
 	# setup paths 
 	# @todo wtf are these for ?
-	$admin_relative = $GSADMIN.'/inc/';
-	$lang_relative = $GSADMIN.'/';
+	$admin_relative = $GSADMIN . '/inc/';
+	$lang_relative = $GSADMIN . '/';
 
 	$load['plugin'] = true;
 	$base = true;
@@ -41,16 +41,14 @@
 /* end */
 
 # Include common.php
-include($GSADMIN.'/inc/common.php');
+include($GSADMIN . '/inc/common.php');
 
 # Hook to load page Cache
 exec_action('index-header');
 
 # get page id (url slug) that is being passed via .htaccess mod_rewrite
-if (isset($_GET['id'])){ 
-	$id = str_replace ('..', '', $_GET['id']);
-	$id = str_replace ('/', '', $id);
-	$id = lowercase($id);
+if (isset($_GET['id'])) { 
+	$id = lowercase(str_replace(array('..', '/'), '', $_GET['id']));
 } else {
 	$id = "index";
 }
@@ -59,12 +57,12 @@ if (isset($_GET['id'])){
 $id = exec_filter('indexid', $id);
  // $_GET['id'] = $id; // support for plugins that are checking get?
 
-# define page
-$data_index = null;
-
+// define page
 // apply page data if page id exists
 if (isset($pagesArray[$id])) {
-	$data_index = getXml(GSDATAPAGESPATH.$id.'.xml');
+	$data_index = getXml(GSDATAPAGESPATH . $id . '.xml');
+} else {
+	$data_index = null;
 }
 
 // filter to modify data_index obj
@@ -72,28 +70,28 @@ $data_index = exec_filter('data_index', $data_index);
 
 // page not found handling
 if (!$data_index) {
-	if (file_exists(GSDATAOTHERPATH.'404.xml')) {
+	if (file_exists(GSDATAOTHERPATH . '404.xml')) {
 		// default 404
-		$data_index = getXml(GSDATAOTHERPATH.'404.xml');
+		$data_index = getXml(GSDATAOTHERPATH . '404.xml');
 	} else {
 		// fail over
 		redirect('404');
 	}
 	exec_action('error-404');
-	header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+	header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
 }
 
 // if page is private, check user
 if ($data_index->private == 'Y' && !is_logged_in()) {
-	if (file_exists(GSDATAOTHERPATH.'403.xml')) {
+	if (file_exists(GSDATAOTHERPATH . '403.xml')) {
 		// default 403
-		$data_index = getXml(GSDATAOTHERPATH.'403.xml');
+		$data_index = getXml(GSDATAOTHERPATH . '403.xml');
 	} else {
 		// fail over
 		redirect('403');
 	}
 	exec_action('error-403');
-	header($_SERVER["SERVER_PROTOCOL"]." 403 Forbidden");
+	header($_SERVER["SERVER_PROTOCOL"] . " 403 Forbidden");
 }
 
 $title         = $data_index->title;
@@ -117,18 +115,18 @@ if (getDef('GSCANONICAL', true)) {
 }
 
 # include the functions.php page if it exists within the theme
-if (file_exists(GSTHEMESPATH .$TEMPLATE."/functions.php")) {
-	include(GSTHEMESPATH .$TEMPLATE."/functions.php");
+if (file_exists(GSTHEMESPATH . $TEMPLATE . '/functions.php')) {
+	include(GSTHEMESPATH . $TEMPLATE . '/functions.php');
 }
 
 # call pretemplate Hook
 exec_action('index-pretemplate');
 
 # include the template and template file set within theme.php and each page
-if ((!file_exists(GSTHEMESPATH.$TEMPLATE."/".$template_file)) || ($template_file == '')) { $template_file = "template.php"; }
-include(GSTHEMESPATH .$TEMPLATE."/".$template_file);
+if ((!file_exists(GSTHEMESPATH . $TEMPLATE . '/' . $template_file)) || ($template_file == '')) {
+	$template_file = 'template.php';
+}
+include(GSTHEMESPATH . $TEMPLATE . '/' . $template_file);
 
 # call posttemplate Hook
 exec_action('index-posttemplate');
-
-?>
