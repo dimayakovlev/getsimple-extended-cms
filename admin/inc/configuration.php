7a7c8795666fd7d9e6a7b34275db9b8c96a11a83
Dmitry Yakovlev <web-dimayakovlev@outlook.com>
2021-05-26 17:58:41 +0300

    Update configuration.php

--- admin/inc/configuration.php
+++ admin/inc/configuration.php
@@ -10,7 +10,7 @@ $site_full_name     = 'GetSimple Extended CMS';
 $site_version_no    = '3.5.0a';
 // $name_url_clean     = lowercase(str_replace(' ' , '-', $site_full_name));
 // $ver_no_clean       = str_replace('.', '', $site_version_no);
-$site_link_back_url = 'http://get-simple.info/';
+$site_link_back_url = 'https://github.com/dimayakovlev/getsimple-extended-cms';
 
 // cookie config
 //$cookie_name        = lowercase($name_url_clean) . '_cookie_' . $ver_no_clean; // non-hashed name of cookie
