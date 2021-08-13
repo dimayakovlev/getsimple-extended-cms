ba12664788473b9143c72064b1de4ba90035f469
Dmitry Yakovlev <web-dimayakovlev@outlook.com>
2021-08-13 16:59:04 +0300

    Fix notice in function toBytes()
    Fix notice "A none well formed numeric value" caused in function toBytes().
    
    Close gh-34.

--- admin/inc/basic.php
+++ admin/inc/basic.php
@@ -1084,20 +1084,23 @@ function get_site_version($echo = true) {
 
 /**
  * Convert to Bytes
+ * Convert M/G/K byte string to bytes
  *
  * @since 3.0
+ * @since 3.5.0 Fix notice
  *
- * @param $str string
- * @return string
+ * @param mixed $str
+ * @return mixed
  */
 function toBytes($str) {
-	$val = trim($str);
-	$last = strtolower($str[strlen($str)-1]);
-		switch($last) {
-			case 'g': $val *= 1024;
-			case 'm': $val *= 1024;
-			case 'k': $val *= 1024;
-		}
+	if (is_numeric($str)) return (int)$str;
+	$val = (int)trim($str, 'gmkGMK');
+	$last = strtolower($str[strlen($str) - 1]);
+	switch ($last) {
+		case 'g': $val *= 1024;
+		case 'm': $val *= 1024;
+		case 'k': $val *= 1024;
+	}
 	return $val;
 }
 
