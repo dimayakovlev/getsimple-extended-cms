5deb5bb97c3dff7190be3b1435dcf0e3274ea4a3
Dmitry Yakovlev <web-dimayakovlev@outlook.com>
2021-09-02 11:53:23 +0300

    Add container to Debug Console
    Add container #debug-console to wrap Debug Console.

--- admin/template/footer.php
+++ admin/template/footer.php
@@ -7,35 +7,31 @@
 ?>
 		<div id="footer">
 			<div class="footer-content">
-      		<?php
-	  		include(GSADMININCPATH . 'configuration.php');
-	  		if (cookie_check()) {
-	  			echo '<p><a href="pages.php">'.i18n_r('PAGE_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="upload.php">'.i18n_r('FILE_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="theme.php">'.i18n_r('THEME_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="backups.php">'.i18n_r('BAK_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="plugins.php">'.i18n_r('PLUGINS_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="settings.php">'.i18n_r('GENERAL_SETTINGS').'</a> &nbsp;&bull;&nbsp; <a href="support.php">'.i18n_r('SUPPORT').'</a></p>';
-	  		}
-      		if (!isAuthPage()) { ?>
-	      		<p>&copy; 2009 - <?php echo date('Y'); ?> <a href="<?php echo $site_link_back_url; ?>" target="_blank"><?php echo $site_full_name; ?></a><?php echo ' &ndash; ' . i18n_r('VERSION') . ' ' . $site_version_no;  ?></p>
-			<?php
-				get_scripts_backend(true);
-				exec_action('footer');
-			}
-			?>
+<?php
+	include(GSADMININCPATH . 'configuration.php');
+	if (cookie_check()) echo '<p><a href="pages.php">' . i18n_r('PAGE_MANAGEMENT') . '</a> &nbsp;&bull;&nbsp; <a href="upload.php">' . i18n_r('FILE_MANAGEMENT') . '</a> &nbsp;&bull;&nbsp; <a href="theme.php">' . i18n_r('THEME_MANAGEMENT') . '</a> &nbsp;&bull;&nbsp; <a href="backups.php">' . i18n_r('BAK_MANAGEMENT') . '</a> &nbsp;&bull;&nbsp; <a href="plugins.php">' . i18n_r('PLUGINS_MANAGEMENT') . '</a> &nbsp;&bull;&nbsp; <a href="settings.php">' . i18n_r('GENERAL_SETTINGS') . '</a> &nbsp;&bull;&nbsp; <a href="support.php">' . i18n_r('SUPPORT') . '</a></p>';
+	if (!isAuthPage()) {
+		echo '<p>&copy; 2009 - ' . date('Y') . '<a href="' . $site_link_back_url . '" target="_blank">' . $site_full_name . '</a> &ndash; ' . i18n_r('VERSION') . ' ' . $site_version_no . '</p>';
+		get_scripts_backend(true);
+		exec_action('footer');
+	}
+?>
 			</div> <!-- end .footer-content -->
-
 		</div><!-- end #footer -->
-		<?php 
-			if (!isAuthPage() && isDebug()) {
-				global $GS_debug;
-				echo '<h2>' . i18n_r('DEBUG_CONSOLE') . '</h2><div id="gsdebug"><pre>';
-				foreach ($GS_debug as $log) {
-					if (is_array($log)) {
-						print_r($log) . '<br/>';
-					} else {
-						print($log . '<br/>');
-					}
-				}
-				echo '</pre></div>';
+<?php
+	if (!isAuthPage() && isDebug()) {
+		global $GS_debug;
+		echo '<div id="debug-console"><h2>' . i18n_r('DEBUG_CONSOLE') . '</h2><div id="gsdebug"><pre>';
+		foreach ($GS_debug as $log) {
+			if (is_array($log)) {
+				print_r($log) . '<br/>';
+			} else {
+				print($log . '<br/>');
 			}
-		?>
+		}
+		echo '</pre></div></div>';
+	}
+?>
 	</div><!-- end .wrapper -->
 </body>
 </html>
