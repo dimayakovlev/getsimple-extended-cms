cdc13e0d7fe977dae50af598ff6791541fe9a4ea
Dmitry Yakovlev <web-dimayakovlev@outlook.com>
2024-11-05 12:10:03 +0300

    Remove CMS version check on Website Health Check
    Part gh-100.

--- admin/health-check.php
+++ admin/health-check.php
@@ -28,27 +28,7 @@ get_template('header', cl($SITENAME).' &raquo; '.i18n_r('SUPPORT').' &raquo; '.i
 		<div class="main">
 			<h3><?php echo $site_full_name; ?></h3>
 			<table class="highlight healthcheck">
-				<?php
-				
-				# check to see if there is a core update needed
-				$data = get_api_details();
-				if ($data)	{
-					$apikey = json_decode($data);
-					$verstatus = $apikey->status;
-				}	else {
-					$verstatus = null;
-				}
-				if ($verstatus == '0') {
-					$ver = '<span class="ERRmsg" ><b>'.$site_version_no.'</b><br /> '. i18n_r('UPG_NEEDED').' (<b>'.$apikey->latest .'</b>)<br /><a href="http://get-simple.info/download/">'. i18n_r('DOWNLOAD').'</a></span>';
-				} elseif ($verstatus == '1') {
-					$ver = '<span class="OKmsg" ><b>'.$site_version_no.'</b><br />'. i18n_r('LATEST_VERSION').'</span>';
-				} elseif ($verstatus == '2') {
-					$ver = '<span class="INFOmsg" ><b>'.$site_version_no.'</b><br /> '. i18n_r('BETA').'</span>';
-				} else {
-					$ver = '<span class="WARNmsg" ><b>'.$site_version_no.'</b><br />'. i18n_r('CANNOT_CHECK').'<br /><a href="http://get-simple.info/download">'. i18n_r('DOWNLOAD').'</a></span>';
-				}
-				?>
-				<tr><td style="width:445px;" ><?php echo $site_full_name; ?> <?php i18n('VERSION');?></td><td><?php echo $ver; ?></td></tr>
+				<tr><td style="width: 445px;"><?php echo $site_full_name; ?> <?php i18n('VERSION'); ?></td><td><span class="OKmsg"><b><?php echo $site_version_no; ?></b></span></td></tr>
                 <?php 
                 if(defined('GSADMIN') && GSADMIN!='admin') echo '<tr><td>GSADMIN</td><td><span class="hint">'.GSADMIN.'</span></td></tr>'; 
                 
