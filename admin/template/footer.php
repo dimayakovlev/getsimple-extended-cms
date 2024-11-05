f523371f628494b0bbad70d78be997af2f86a292
Dmitry Yakovlev <web-dimayakovlev@outlook.com>
2024-11-05 13:28:19 +0300

    Use constant GSNAME in admin panel
    Part of gh-100.

--- admin/template/footer.php
+++ admin/template/footer.php
@@ -7,25 +7,23 @@
 
 ?>
 		<div id="footer">
-      		<div class="footer-left" >
+      		<div class="footer-left">
       		<?php 
-	  		include(GSADMININCPATH ."configuration.php");
+	  		include(GSADMININCPATH . 'configuration.php');
 	  		if (cookie_check()) { 
 	  			echo '<p><a href="pages.php">'.i18n_r('PAGE_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="upload.php">'.i18n_r('FILE_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="theme.php">'.i18n_r('THEME_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="backups.php">'.i18n_r('BAK_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="plugins.php">'.i18n_r('PLUGINS_MANAGEMENT').'</a> &nbsp;&bull;&nbsp; <a href="settings.php">'.i18n_r('GENERAL_SETTINGS').'</a> &nbsp;&bull;&nbsp; <a href="support.php">'.i18n_r('SUPPORT').'</a></p>';
 	  		}
       		
       		if(!isAuthPage()){ ?>
-	      		<p><?php i18n('POWERED_BY'); ?> <a href="<?php echo var_out($site_link_back_url, 'url'); ?>" target="_blank" ><?php echo var_out($site_full_name); ?></a>
-	      		<?php echo '&ndash; '. i18n_r('VERSION') .' '. $site_version_no;  ?>
-	      		</p> 
+	      		<p><?php i18n('POWERED_BY'); ?> <a href="<?php echo var_out($site_link_back_url, 'url'); ?>" target="_blank"><?php echo GSNAME; ?></a><?php echo ' &ndash; ' . i18n_r('VERSION') . ' ' . GSVERSION; ?></p>
       		</div> <!-- end .footer-left -->
-	      	<div class="gslogo" >
-		      	<a href="http://get-simple.info/" target="_blank" ><img src="template/images/getsimple_logo.gif" alt="GetSimple Content Management System" /></a>
+	      	<div class="gslogo">
+		      	<a href="http://get-simple.info/" target="_blank"><img src="template/images/getsimple_logo.gif" alt="GetSimple Content Management System" /></a>
 		    </div>
 	      	<div class="clear"></div>
 	      	<?php
 		      		get_scripts_backend(TRUE);
-		      		exec_action('footer'); 
+		      		exec_action('footer');
 	      		}
 	      	?>
 
